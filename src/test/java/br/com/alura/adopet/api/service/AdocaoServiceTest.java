c8748f885c297c716c78c7fe4cce4d57342af55f
caio.amaro <caioamarocci@gmail.com>
2025-05-29 09:33:41 -0300

    faz teste em service

--- src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
+++ src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -1,7 +1,61 @@
 package br.com.alura.adopet.api.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import br.com.alura.adopet.api.repository.PetRepository;
+import br.com.alura.adopet.api.repository.TutorRepository;
+import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
 class AdocaoServiceTest {
+
+    @InjectMocks
+    private AdocaoService adocaoService;
+
+    @Mock
+    private AdocaoRepository Adocaorepository;
+
+    @Mock
+    private PetRepository petRepository;
+
+    @Mock
+    private TutorRepository tutorRepository;
+
+    @Mock
+    private EmailService emailService;
+
+    @Mock
+    private List<ValidacaoSolicitacaoAdocao> validacoes;
+
+    @Mock
+    private Abrigo abrigo;
+
+    @Mock
+    private SolicitacaoAdocaoDto dto;
+
+    @Test
+    void deveriaSalvarAdocaoAoSolicitar(){
+        //ARRANGE
+
+        //ACT
+        adocaoService.solicitar(dto);
+
+        //ASSERT
+        then(Adocaorepository).should().save(any());
+
+    }
 
 }
